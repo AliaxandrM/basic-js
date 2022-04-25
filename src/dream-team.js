d186f82c4edc7182df1f1719fe2c0b6c87941bea
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2022-04-25 23:48:26 +0300

    feat: implement functiom dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+    const arr = members;
+    if (!Array.isArray(arr)) {
+        return false;
+    }
+    if (arr.length === 0) {
+        return null;
+    }
+    let resStr = '';
+    arr.forEach(i => {
+        if (typeof i === 'string') {
+            let itenStr = i;
+                while (itenStr.slice(0,1) == ' ') {
+                    itenStr = itenStr.slice(1, itenStr.length);
+                }
+            resStr = resStr + itenStr.slice(0,1).toUpperCase();
+        }   
+    })
+    function bubbleSort(arr) {
+        for (var i = 0, endI = arr.length - 1; i < endI; i++) {
+            for (var j = 0, endJ = endI - i; j < endJ; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    var swap = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = swap;
+                }
+            }
+        }
+        return arr;
+    }
+    resArr = resStr.split('');
+    let sortedArr = bubbleSort(resArr);
+    const result = sortedArr.join('');
+    return result;
 }
 
 module.exports = {
